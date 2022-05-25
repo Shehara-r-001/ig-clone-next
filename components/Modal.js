649a28f4aec98e8d28f497c2686dbe6b818e32c4
Shehara_r <ranathisarashehara01@gmail.com>
2022-05-25 14:06:40 +0530

    modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,10 +1,26 @@
 import { Dialog, Transition } from '@headlessui/react';
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
+import { BsCamera } from 'react-icons/bs';
 
 const Modal = () => {
   const [open, setOpen] = useRecoilState(modalState);
+  const filePickerRef = useRef(null);
+  const captionRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const addImage = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result);
+    };
+  };
+
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
@@ -26,7 +42,7 @@ const Modal = () => {
           </Transition.Child>
           <span
             className='hidden sm:inline-block sm:align-middle sm:h-screen'
-            area-hidden='true'
+            aria-hidden='true'
           >
             &#8203;
           </span>
@@ -40,7 +56,60 @@ const Modal = () => {
             leaveFrom='oopacity-100 translate-y-4 sm:translate-y-0 sm:scale-95'
           >
             <div className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'>
-              <h1>Hello</h1>
+              <div>
+                {selectedFile ? (
+                  <img
+                    src={selectedFile}
+                    className='w-full object-contain cursor-pointer'
+                    onClick={() => setSelectedFile(null)}
+                    alt=''
+                  />
+                ) : (
+                  <div
+                    onClick={() => filePickerRef.current.click()}
+                    className='mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-pink-100 cursor-pointer'
+                  >
+                    <BsCamera
+                      className='h-6 w-6 text-red-600'
+                      aria-hidden='true'
+                    />
+                  </div>
+                )}
+                <div>
+                  <div className='mt-3 text-center sm:mt-5'>
+                    <Dialog.Title
+                      as='h3'
+                      className='text-lg leading-6 font-medium text-gray-900'
+                    >
+                      Upload a photo
+                    </Dialog.Title>
+                    <div>
+                      <input
+                        ref={filePickerRef}
+                        type='file'
+                        hidden
+                        onChange={addImage}
+                      />
+                    </div>
+                    <div className='mt-2'>
+                      <input
+                        type='text'
+                        className='border-none focus:ring-0 w-full text-center'
+                        placeholder='Please enter a caption'
+                        ref={captionRef}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='mt-5 sm:mt-6'>
+                  <button
+                    type='button'
+                    className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-pink-600 text-base font-medium text-white hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500 sm:text-sm disabled:bg-gray-300 disabled:cursor-not-allowed hiver:disabled:bg-gray-300'
+                  >
+                    Upload Post
+                  </button>
+                </div>
+              </div>
             </div>
           </Transition.Child>
         </div>
