bf334d2493097d14e00a6ef215bf49c8d4ba19fb
Shehara_r <ranathisarashehara01@gmail.com>
2022-05-26 20:46:57 +0530

    data retreiving from firebase

--- components/Posts.js
+++ components/Posts.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import Post from './Post';
 
-const posts = [
-  {
-    id: 1,
-    username: 'strider',
-    avatar: 'https://avatars.githubusercontent.com/u/84827162?v=4',
-    img: 'https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/apex-legends-mobile-faq/common/apex-mobile-announce-art-3840x2160.jpg.adapt.crop16x9.575p.jpg',
-    caption: '#ApexLegends',
-  },
-  {
-    id: 2,
-    username: 'strider',
-    // avatar: 'https://i.pravatar.cc/300?img=1',
-    avatar: 'https://avatars.githubusercontent.com/u/84827162?v=4',
-    img: 'https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/apex-legends-mobile-faq/common/apex-mobile-announce-art-3840x2160.jpg.adapt.crop16x9.575p.jpg',
-    caption: '#ApexLegends',
-  },
-];
+// const posts = [
+//   {
+//     id: 1,
+//     username: 'strider',
+//     avatar: 'https://avatars.githubusercontent.com/u/84827162?v=4',
+//     img: 'https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/apex-legends-mobile-faq/common/apex-mobile-announce-art-3840x2160.jpg.adapt.crop16x9.575p.jpg',
+//     caption: '#ApexLegends',
+//   },
+//   {
+//     id: 2,
+//     username: 'strider',
+//     // avatar: 'https://i.pravatar.cc/300?img=1',
+//     avatar: 'https://avatars.githubusercontent.com/u/84827162?v=4',
+//     img: 'https://media.contentapi.ea.com/content/dam/apex-legends/common/articles/apex-legends-mobile-faq/common/apex-mobile-announce-art-3840x2160.jpg.adapt.crop16x9.575p.jpg',
+//     caption: '#ApexLegends',
+//   },
+// ];
 
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
-          username={post.username}
-          avatar={post.avatar}
-          caption={post.caption}
-          image={post.img}
+          username={post.data().username}
+          avatar={post.data().profileImg}
+          caption={post.data().caption}
+          image={post.data().image}
         />
       ))}
     </div>
